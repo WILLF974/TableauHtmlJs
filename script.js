226a0bdcb535db2e71fc27e9c26d9e14e4df3383
riviere wilfrid <riviere.will@gmail.com>
2024-08-05 20:29:18 +0400

    Avec tableau

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const XLSX = require('xlsx');
 
 /* Script pour lire le fichier et afficher les données */
 
@@ -9,16 +8,36 @@ function handleFile(e) {
     const reader = new FileReader();
     reader.onload = function(event) {
         const data = new Uint8Array(event.target.result);
-        const workbook = XLSX.read(data, {type: 'array'});
+        const workbook = XLSX.read(data, { type: 'array' });
         const firstSheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[firstSheetName];
-        const json = XLSX.utils.sheet_to_json(worksheet, {header: 1});
-        displayData(json);
+        const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+        displayTable(json);
     };
     reader.readAsArrayBuffer(file);
 }
 
-function displayData(data) {
+function displayTable(data) {
     const output = document.getElementById('output');
-    output.textContent = JSON.stringify(data, null, 2);
+    let html = '<table border="1"><thead><tr>';
+
+    // Ajouter les en-têtes de colonne
+    data[0].forEach(header => {
+        html += `<th>${header}</th>`;
+    });
+
+    html += '</tr></thead><tbody>';
+
+    // Ajouter les lignes de données
+    for (let i = 1; i < data.length; i++) {
+        html += '<tr>';
+        data[i].forEach(cell => {
+            html += `<td>${cell}</td>`;
+        });
+        html += '</tr>';
+    }
+
+    html += '</tbody></table>';
+    output.innerHTML = html;
 }
+
